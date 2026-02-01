0f7a8cbc5685c6df4c48c74551de6f561690a3f9
pranavbhagat1781-sudo <pranavbhagat1781@gmail.com>
2026-02-01 23:14:23 +0530

    Change yes button response and add image
    Updated the text displayed when the 'yes' button is clicked and added an image.

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const question = document.getElementById("question");
+const image = document.getElementById("mainImage");
 
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * 200 - 100;
     const y = Math.random() * 200 - 100;
-    noBtn.style.transform = `translate(${x}px, ${y}px)`;
+    noBtn.style.transform = translate(${x}px, ${y}px);
 });
 
 yesBtn.addEventListener("click", () => {
-    question.innerHTML = "Will You be my Valintine YAY!!! 💖🥰 ";
+    question.innerHTML = "YAY!!! 💖🥰 I love you YASHITA!";
+    image.src =
+      "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExY2JuYjZyOGszdXlzMnN1dWtiNXYyZ3Jscmpvb3kydjMwbnhxYjlqayZlcD12MV9naWZzX3NlYXJjaCZjdD1n/2rqEdFfkMzXmo/giphy.gif";
     document.querySelector(".buttons").style.display = "none";
 });
