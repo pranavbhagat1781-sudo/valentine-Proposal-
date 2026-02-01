51c61967e0c48ca2d332a8dbe3720a25500fcbca
pranavbhagat1781-sudo <pranavbhagat1781@gmail.com>
2026-02-01 23:38:14 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 const question = document.getElementById("question");
-const image = document.getElementById("mainImage");
 
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * 200 - 100;
     const y = Math.random() * 200 - 100;
-    noBtn.style.transform = translate(${x}px, ${y}px);
+    noBtn.style.transform = `translate(${x}px, ${y}px)`;
 });
 
 yesBtn.addEventListener("click", () => {
     question.innerHTML = "YAY!!! 💖🥰 I love you YASHITA!";
-    image.src =
-      "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdzVmOGFpNzl6c3V1ZWczaDJtd3JibTNpZjhvMDk2ejk1NjEydms4cSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/75ZaxapnyMp2w/giphy.gif";
     document.querySelector(".buttons").style.display = "none";
 });
